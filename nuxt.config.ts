8a0735f1047577d469320cc21c7926e83d90395a
Horace <horacekeung@hotmail.com>
2025-02-21 03:49:00 +0800

    try deploy 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	ssr: false,
 	app: {
 		// baseURL: '/portfolio',
 		cdnURL: 'https://jaswando.github.io/portfolio/',
