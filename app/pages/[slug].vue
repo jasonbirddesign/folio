02c5001329fe542f90a827e7fd9a66cca7c5daa9
Horace <horacekeung@hotmail.com>
2025-03-02 06:22:21 +0800

    rich text

--- app/pages/[slug].vue
+++ app/pages/[slug].vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
+import { BLOCKS } from '@contentful/rich-text-types'
 const route = useRoute()
 const { $work } = useNuxtApp()
 const work = computed<Work>(() => {
@@ -6,13 +8,30 @@ const work = computed<Work>(() => {
 		return item.fields.slug === route.params.slug
 	})?.fields
 })
+const html = computed(() => {
+	return documentToHtmlString(work.value.body, {
+		renderNode: {
+			[BLOCKS.EMBEDDED_ASSET]: (node) => {
+				return `<img src="${node.data.target.fields.file.url}" alt="${node.data.target.fields.title}" />`
+			}
+		}
+	})
+})
 </script>
 
 <template>
-	<div
-		v-if="work"
-		class="container"
-	>
-		<WorkHero :work/>
-	</div>
+	<template v-if="work">
+		<div class="container py-22.5 gap-16">
+			<WorkHero :work/>
+			<div
+				v-if="html"
+				v-html="html"
+			></div>
+		</div>
+		<WorkNext
+			v-if="work.nextWorkTitle && work.nextWorkSlug"
+			:title="work.nextWorkTitle"
+			:slug="work.nextWorkSlug"
+		/>
+	</template>
 </template>
