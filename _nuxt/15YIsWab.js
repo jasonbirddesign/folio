a3d4673a2a4f72e0627c1c46c609e2aa6caf50f9
HoraceKeung <horacekeung@hotmail.com>
2025-03-01 16:00:10 +0000

    Deploying to gh-pages from @ jasonbirddesign/folio@399b78b6ba0e89e6e3345c4220dc765dcc2d5ccf 🚀

---
+++ _nuxt/15YIsWab.js
@@ -0,0 +1,1 @@
+import{m as t,u as n,c as o,o as s,t as a,n as r}from"./DvWnHcGe.js";const _=t({__name:"[...slug]",setup(u){const e=n();return(c,p)=>(s(),o("div",null,a(r(e).path),1))}});export{_ as default};
