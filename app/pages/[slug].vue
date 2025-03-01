1a613a67026cbe9ca2f1d18f9c3142844484699f
Horace <horacekeung@hotmail.com>
2025-03-02 04:42:47 +0800

    new work hero

---
+++ app/pages/[slug].vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const route = useRoute()
+const { $work } = useNuxtApp()
+const work = computed<Work>(() => {
+	return $work?.items?.find(item => {
+		return item.fields.slug === route.params.slug
+	})?.fields
+})
+</script>
+
+<template>
+	<div
+		v-if="work"
+		class="container"
+	>
+		<WorkHero :work/>
+	</div>
+</template>
